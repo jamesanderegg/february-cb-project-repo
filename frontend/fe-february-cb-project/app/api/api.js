162708047e1237ffad6e34a795888439bced0110
poorvarpatel <poorvarpatel@gmail.com>
2025-02-02 21:34:46 -0500

    worked on the three.js file

--- frontend/fe-february-cb-project/app/api/api.js
+++ frontend/fe-february-cb-project/app/api/api.js
@@ -1,9 +1,12 @@
-const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL || 'http://localhost:5000';
+// const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL || 'http://localhost:5000';
+
+const API_BASE_URL = 'http://localhost:5000';  // Hardcoded for testing
+console.log("API Base URL:", API_BASE_URL);
 
 console.log(API_BASE_URL)
 export async function getFlaskData() {
   try {
-    const response = await fetch(`${API_BASE_URL}/`);
+    const response = await fetch(`${API_BASE_URL}`);
     if (!response.ok) {
       throw new Error('Failed to fetch data from Flask API');
     }
