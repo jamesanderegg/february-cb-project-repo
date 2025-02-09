7e25ea541f3988bf3fdd2c931cf90242cb746175
poorvarpatel <poorvarpatel@gmail.com>
2025-02-08 20:08:17 -0500

    table in three to be converted to r3f

---
+++ frontend/src/scene/objects/Table.js
@@ -0,0 +1,20 @@
+//Table Construction
+const table = new THREE.Group();
+
+const tableBaseGeometry = new THREE.CylinderGeometry(0.25, 0.75, 0.25);
+const tableMaterial = new THREE.MeshBasicMaterial({color:'white', side: THREE.DoubleSide});
+const tableBase = new THREE.Mesh(tableBaseGeometry, tableMaterial);
+tableBase.position.set(0, 0.125, 0);
+tableBase.add(table);
+
+const tableStandGeometry = new THREE.CylinderGeometry(0, 0.15, 1);
+const tableStand = new THREE.Mesh(tableStandGeometry, tableMaterial);
+tableStand.position.set(-0, 0.625, 0);
+tableStand.add(table);
+
+const tableTopGeometry = new THREE.BoxGeometry(1.4, 0.025, 1.4);
+const tableTop = new THREE.Mesh(tableTopGeometry, tableMaterial);
+tableTop.position.set(0, 1.125, 0);
+tableTop.add(table);
+
+scene.add(table);
